e1c827410a3e49cb056ab1f8e8acf2df932ab21a
Ishari <panduishari26@gmail.com>
2022-03-30 08:21:04 +0700

    update primary color

--- lib/constant.dart
+++ lib/constant.dart
@@ -1,4 +1,4 @@
 import 'package:flutter/material.dart';
 
-const bPrimaryColor       =Color(0xFF3c8187);
+const bPrimaryColor       =Color(0xFF00838f);
 const bPrimaryLightColor  =Color(0xFFFFECDF);
