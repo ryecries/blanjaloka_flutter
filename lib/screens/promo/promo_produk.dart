090f7b315a6fa07a6e857260234d49a09d3c8e54
ryecries <you@example.com>
2022-04-28 06:05:15 +0700

    add DatePicker

--- lib/screens/promo/promo_produk.dart
+++ lib/screens/promo/promo_produk.dart
@@ -1,6 +1,7 @@
 import 'package:blanjaloka_flutter/constant.dart';
 import 'package:blanjaloka_flutter/widgets/primary_button.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class PagePromoToko extends StatefulWidget {
   const PagePromoToko({Key? key}) : super(key: key);
@@ -10,6 +11,18 @@ class PagePromoToko extends StatefulWidget {
 }
 
 class _PagePromoTokoState extends State<PagePromoToko> {
+
+  TextEditingController datestartinput = TextEditingController();
+  TextEditingController dateendinput = TextEditingController();
+  //text editing controller for text field
+
+  @override
+  void initState() {
+    datestartinput.text = ""; //set the initial value of text field
+    dateendinput.text = ""; //set the initial value of text field
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -122,38 +135,43 @@ class _PagePromoTokoState extends State<PagePromoToko> {
                             fontSize: 14,
                           ),
                         ),
-                        InkWell(
-                          splashColor: bPrimaryColor,
-                          child: Align(
-                            alignment: Alignment.centerLeft,
-                            child: Container(
-                              width: double.infinity,
-                              child: Padding(
-                                padding: const EdgeInsets.symmetric(vertical: 2),
-                                child: Row(
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceBetween,
-                                  children: [
-                                    Text(
-                                      "Pilih Tanggal",
-                                      style: TextStyle(color: Colors.grey),
-                                    ),
-                                    SizedBox(height: 45),
-                                    Padding(
-                                      padding: const EdgeInsets.only(right: 15),
-                                      child: Icon(
-                                          Icons.keyboard_arrow_down_rounded,
-                                          size: 16,
-                                          color: Colors.grey),
-                                    )
-                                  ],
-                                ),
+                        Align(
+                          alignment: Alignment.centerLeft,
+                          child: Container(
+                            width: double.infinity,
+                            child: TextField(
+                              controller: datestartinput,
+                              decoration: InputDecoration(
+                                  hintText: "Pilih Tangal",
+                                  suffixIcon: Icon(
+                                    Icons.keyboard_arrow_down_rounded,
+                                    size: 16,
+                                  )
                               ),
+                              readOnly: true,
+                              onTap: () async {
+                                DateTime? pickedDate = await showDatePicker(
+                                    context: context, initialDate: DateTime.now(),
+                                    firstDate: DateTime(2000), //DateTime.now() - not to allow to choose before today.
+                                    lastDate: DateTime(2101)
+                                );
+
+                                if(pickedDate != null ){
+                                  print(pickedDate);  //pickedDate output format => 2021-03-10 00:00:00.000
+                                  String formattedDate = DateFormat('E, dd MMM yyyy').format(pickedDate);
+                                  print(formattedDate); //formatted date output using intl package =>  2021-03-16
+                                  //you can implement different kind of Date Format here according to your requirement
+
+                                  setState(() {
+                                    datestartinput.text = formattedDate; //set output date to TextField value.
+                                  });
+                                }else{
+                                  print("Date is not selected");
+                                }
+                              },
                             ),
                           ),
-                          onTap: () {
-                          },
-                        )
+                        ),
                       ],
                     ),
                   ),
@@ -176,37 +194,44 @@ class _PagePromoTokoState extends State<PagePromoToko> {
                             fontSize: 14,
                           ),
                         ),
-                        InkWell(
-                          splashColor: bPrimaryColor,
-                          child: Align(
+                        Align(
                             alignment: Alignment.centerLeft,
                             child: Container(
                               width: double.infinity,
-                              child: Padding(
-                                padding: const EdgeInsets.symmetric(vertical: 2),
-                                child: Row(
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceBetween,
-                                  children: [
-                                    Text(
-                                      "Pilih Tanggal",
-                                      style: TextStyle(color: Colors.grey),
-                                    ),
-                                    SizedBox(height: 45),
-                                    Padding(
-                                      padding: const EdgeInsets.only(right: 15),
-                                      child: Icon(
-                                          Icons.keyboard_arrow_down_rounded,
-                                          size: 16,
-                                          color: Colors.grey),
+                              child: TextField(
+                                controller: dateendinput,
+                                decoration: InputDecoration(
+                                    hintText: "Pilih Tangal",
+                                    suffixIcon: Icon(
+                                      Icons.keyboard_arrow_down_rounded,
+                                      size: 16,
                                     )
-                                  ],
                                 ),
+                                readOnly: true,
+                                onTap: () async {
+                                  DateTime? pickedDate = await showDatePicker(
+                                      context: context, initialDate: DateTime.now(),
+                                      firstDate: DateTime(2000), //DateTime.now() - not to allow to choose before today.
+                                      lastDate: DateTime(2101)
+                                  );
+
+                                  if(pickedDate != null ){
+                                    print(pickedDate);  //pickedDate output format => 2021-03-10 00:00:00.000
+                                    String formattedDate = DateFormat('E, dd MMM yyyy').format(pickedDate);
+                                    print(formattedDate); //formatted date output using intl package =>  2021-03-16
+                                    //you can implement different kind of Date Format here according to your requirement
+
+                                    setState(() {
+                                      dateendinput.text = formattedDate; //set output date to TextField value.
+                                    });
+                                  }else{
+                                    print("Date is not selected");
+                                  }
+                                },
                               ),
                             ),
                           ),
-                          onTap: () {},
-                        )
+
                       ],
                     ),
                   ),
